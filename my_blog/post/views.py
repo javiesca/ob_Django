5bbfa43a8cf4a9ff9dca946f59a09f59ad1e2a9c
Javier Viesca Tuñón <viesca@gmail.com>
2022-07-26 18:50:19 +0200

    Curso Django OpenBootcamp

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -20,5 +20,15 @@ def queries(request):
 
     #Saltando los elementos que queramos (10 elementos saltando los 5 primeros)
     offsets = Author.objects.all()[5:15]
-                                                #CONTESTO
-    return render(request, 'post/queries.html', {'authors':authors, 'filtered':filtered, 'author':author, 'limits':limits, 'offsets':offsets})
+
+    #METODOS DE ORDENACION
+    orders = Author.objects.all().order_by('email')
+
+    #Obtener todos los elementos cuyo id sea menor o igual a 15
+    filtered2 = Author.objects.filter(id__lte=15)
+
+    #Obtener todos los autores que contienen en su nombre la palabra 'music'
+    filtered3 = Author.objects.filter(name__contains="music")
+
+                                                #CONTEXTO
+    return render(request, 'post/queries.html', {'authors':authors, 'filtered':filtered, 'author':author, 'limits':limits, 'offsets':offsets, 'orders':orders, 'filtered2':filtered2, 'filtered3':filtered3})
