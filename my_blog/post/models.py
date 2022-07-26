50940f8aa5cb6b91207cd38ba3c8afc39798c8e2
Javier Viesca Tuñón <viesca@gmail.com>
2022-07-26 15:21:47 +0200

    Curso Django OpenBootcamp

--- my_blog/post/models.py
+++ my_blog/post/models.py
@@ -5,7 +5,7 @@ from datetime import date
 
 class Author(models.Model):
 
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     email = models.EmailField(max_length=200)
 
     def __str__(self):
