c6730cc44c197586c544129770f9bddeaf9aceb7
Javier Viesca Tuñón <viesca@gmail.com>
2022-07-29 12:01:29 +0200

    Curso Django de OpenBootcamp

---
+++ relations/onetoone/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+#RELACION UNO A UNO. models.OneToOneField (clase, on_delete=models.CASCADE, primary_key=True)
+class Restaurant(models.Model):
+
+    place = models.OneToOneField(Place, on_delete=models.CASCADE, primary_key=True)
+    number_of_employees = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.place.name
+    
+
+
---
+++ relations/onetoone/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Place, Restaurant
+# Create your views here.
+
+def create (request):
+    place = Place(name="Lugar1", address = "Calle Demo")
+    place.save()
+
+    restaurant = Restaurant(place=place, number_of_employees=5)
+    restaurant.save()
+
+    return HttpResponse(restaurant.place.name)
